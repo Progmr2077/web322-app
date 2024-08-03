f92955ee0fdc20d136866e21655c6eda7515bafb
jrivera17 <jrivera17@myseneca.ca>
2024-08-02 23:54:26 -0400

    Added the files back

---
+++ views/categories.js
@@ -0,0 +1,18 @@
+[
+    {
+       "id":1,
+       "category":"School"
+    },
+    {
+       "id":2,
+       "category":"Electronics"
+    },
+    {
+       "id":3,
+       "category":"Clothing"
+    },
+    {
+       "id":4,
+       "category":"Sports"
+    }
+ ]
