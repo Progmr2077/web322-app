80b53c0a582324b331c39608fb67a3b766300ecb
jrivera17 <jrivera17@myseneca.ca>
2024-08-12 17:06:18 -0400

    fixed code

--- db-setup.js
+++ db-setup.js
@@ -1,13 +1,17 @@
 // db-setup.js
 const mongoose = require('mongoose');
 
-const dbUri = 'mongodb://username:password@host:port/your_database';
+// Use environment variables for sensitive information
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/your_database';
 
 mongoose.connect(dbUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit the process with an error code
+  });
 
 module.exports = mongoose;
