6f04841eec46493d44917a3a3d36fa2a3b387a7b
jrivera17 <jrivera17@myseneca.ca>
2024-08-12 17:05:33 -0400

    formatted the data properly

--- categories.js
+++ categories.js
@@ -1,18 +1,19 @@
 [
-    {
-       "id":1,
-       "category":"School"
-    },
-    {
-       "id":2,
-       "category":"Electronics"
-    },
-    {
-       "id":3,
-       "category":"Clothing"
-    },
-    {
-       "id":4,
-       "category":"Sports"
-    }
+   {
+     "id": 1,
+     "category": "School"
+   },
+   {
+     "id": 2,
+     "category": "Electronics"
+   },
+   {
+     "id": 3,
+     "category": "Clothing"
+   },
+   {
+     "id": 4,
+     "category": "Sports"
+   }
  ]
+ 
