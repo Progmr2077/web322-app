da7d209b3f76c3af9e9d5644a8e875dcbaf737ab
jrivera17 <jrivera17@myseneca.ca>
2024-08-12 12:02:29 -0400

    fixed formatting of code

--- views/items.js
+++ views/items.js
@@ -1,102 +1,102 @@
 [
-    {
-       "id":1,
-       "category":2,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2016/11/19/15/32/laptop-1839876_1280.jpg",
-       "price":1999.99,
-       "title":"MSI Kitana GF66",
-       "body":"Gaming laptop thats affordable and good for coding.",
-       "published":true
-    },
-    {
-       "id":2,
-       "category":1,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2018/04/02/17/35/nature-3284504_1280.jpg",
-       "price":35.99,
-       "title":"Nature School Backpack",
-       "body":"Backpack for school and nature.",
-       "published":true
-    },
-    {
-       "id":3,
-       "category":2,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2022/01/07/10/38/playstation-controller-6921619_1280.jpg",
-       "price":579.99,
-       "title":"PS5 Controller",
-       "body":"PS5 controller for computer or PS5 console.",
-       "published":true
-    },
-    {
-       "id":4,
-       "category":2,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2017/01/23/17/49/xbox-2003277_1280.jpg",
-       "price":459.99,
-       "title":"XBOX Controller",
-       "body":"XBOX controller for computer or XBOX console.",
-       "published":true
-    },
-    {
-       "id":5,
-       "category":3,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2021/12/07/08/35/man-6852605_1280.jpg",
-       "price":30.99,
-       "title":"T-Shirt",
-       "body":"T-shirt for looking casual and stylish.",
-       "published":true
-    },
-    {
-       "id":6,
-       "category":3,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2016/11/29/07/16/balancing-1868051_1280.jpg",
-       "price":25.99,
-       "title":"Shorts",
-       "body":"Shorts for looking casual and stylish.",
-       "published":true
-    },
-    {
-       "id":7,
-       "category":4,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2016/05/30/16/24/baseball-1425124_1280.jpg",
-       "price":9.97,
-       "title":"Baseball",
-       "body":"Baseball to use for baseball games.",
-       "published":true
-    },
-    {
-       "id":8,
-       "category":4,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2014/10/14/20/24/soccer-488700_1280.jpg",
-       "price":10.98,
-       "title":"Soccer Ball",
-       "body":"Soccer ball to use for soccer games.",
-       "published":true
-    },
-    {
-       "id":9,
-       "category":1,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2016/03/27/20/55/arm-1284248_1280.jpg",
-       "price":2.46,
-       "title":"Notebook For School",
-       "body":"Notebook to use for school.",
-       "published":true
-    },
-    {
-       "id":10,
-       "category":1,
-       "postDate":"2024-07-14",
-       "featureImage":"https://cdn.pixabay.com/photo/2016/05/24/17/38/stationery-1412848_1280.jpg",
-       "price":3.99,
-       "title":"Pencil for School",
-       "body":"Mechanical pencil to use for school or drawing.",
-       "published":true
-    }
- ] 
+   {
+       "id": 1,
+       "category": 2,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2016/11/19/15/32/laptop-1839876_1280.jpg",
+       "price": 1999.99,
+       "title": "MSI Kitana GF66",
+       "body": "Gaming laptop that's affordable and good for coding.",
+       "published": true
+   },
+   {
+       "id": 2,
+       "category": 1,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2018/04/02/17/35/nature-3284504_1280.jpg",
+       "price": 35.99,
+       "title": "Nature School Backpack",
+       "body": "Backpack for school and nature.",
+       "published": true
+   },
+   {
+       "id": 3,
+       "category": 2,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2022/01/07/10/38/playstation-controller-6921619_1280.jpg",
+       "price": 579.99,
+       "title": "PS5 Controller",
+       "body": "PS5 controller for computer or PS5 console.",
+       "published": true
+   },
+   {
+       "id": 4,
+       "category": 2,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2017/01/23/17/49/xbox-2003277_1280.jpg",
+       "price": 459.99,
+       "title": "XBOX Controller",
+       "body": "XBOX controller for computer or XBOX console.",
+       "published": true
+   },
+   {
+       "id": 5,
+       "category": 3,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2021/12/07/08/35/man-6852605_1280.jpg",
+       "price": 30.99,
+       "title": "T-Shirt",
+       "body": "T-shirt for looking casual and stylish.",
+       "published": true
+   },
+   {
+       "id": 6,
+       "category": 3,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2016/11/29/07/16/balancing-1868051_1280.jpg",
+       "price": 25.99,
+       "title": "Shorts",
+       "body": "Shorts for looking casual and stylish.",
+       "published": true
+   },
+   {
+       "id": 7,
+       "category": 4,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2016/05/30/16/24/baseball-1425124_1280.jpg",
+       "price": 9.97,
+       "title": "Baseball",
+       "body": "Baseball to use for baseball games.",
+       "published": true
+   },
+   {
+       "id": 8,
+       "category": 4,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2014/10/14/20/24/soccer-488700_1280.jpg",
+       "price": 10.98,
+       "title": "Soccer Ball",
+       "body": "Soccer ball to use for soccer games.",
+       "published": true
+   },
+   {
+       "id": 9,
+       "category": 1,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2016/03/27/20/55/arm-1284248_1280.jpg",
+       "price": 2.46,
+       "title": "Notebook For School",
+       "body": "Notebook to use for school.",
+       "published": true
+   },
+   {
+       "id": 10,
+       "category": 1,
+       "postDate": "2024-07-14",
+       "featureImage": "https://cdn.pixabay.com/photo/2016/05/24/17/38/stationery-1412848_1280.jpg",
+       "price": 3.99,
+       "title": "Pencil for School",
+       "body": "Mechanical pencil to use for school or drawing.",
+       "published": true
+   }
+]
